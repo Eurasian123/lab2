50a6182016afd16c9c87c73b41bca69c2ce1171c
Krishna Mandal <kumandal@student.apc.edu.ph>
2024-01-22 14:01:22 +0800

    Lesson 23 - Browser DOMs (incl. fixes)

--- week5/js/button.js
+++ week5/js/button.js
@@ -1,6 +1,17 @@
 var buttonsfx = new Audio("ost/INSERT DISC.mp3");
+var fire = new Audio("ost/The Fire Is Gone.mp3");
+document.getElementById("btn1").addEventListener("click", insertDisc);
+
+function thirdFunctionGdi() {
+	
+}
+
+function loadScreen() {
+	document.getElementById("ld").style.display = "block";
+}
 
 function insertDisc() {
 	buttonsfx.play();
-	alert("Something will happen soon!");
+	btn1.style.display = "none";
+	setTimeout(loadScreen, 6000);
 }
