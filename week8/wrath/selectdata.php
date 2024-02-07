614fdaf7ba951ddfa5a0b9e734df7e7d5f0153fd
Krishna Mandal <kumandal@student.apc.edu.ph>
2024-02-08 01:54:27 +0800

    gluttony fade finalization

---
+++ week8/wrath/selectdata.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "webprogmi222_sf221";
+$password = "xE*Y2nleNVvZm[!!";
+$dbname = "webprogmi222_sf221";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name, email, website, comment, gender FROM kumandal_myguests;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+echo "Guest: " . $row["name"]. " (" . $row["gender"]. ") - " . $row["email"]. ", " . $row["website"]. "   '" . $row["comment"]. "' ";
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+?>
